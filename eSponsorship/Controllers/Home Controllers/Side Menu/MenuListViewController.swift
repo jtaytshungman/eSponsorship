09a53201c76dedb1fffdd19e1714bd4e9f93ce4f
Jeremy Tay <j.taytshungman@gmail.com>
2017-09-30 00:43:27 +0800

    Working Menu

--- eSponsorship/Controllers/Home Controllers/Side Menu/MenuListViewController.swift
+++ eSponsorship/Controllers/Home Controllers/Side Menu/MenuListViewController.swift
@@ -9,27 +9,21 @@
 import UIKit
 
 class MenuListViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func addTeams(_ sender: Any) {
+//        let storyboard = UIStoryboard(name: "Adding", bundle: Bundle.main)
+//        guard let vc = storyboard.instantiateViewController(withIdentifier: "addTeamsViewController") as? addTeamsViewController else { return }
     }
-    */
-
+    
+    @IBAction func addTournaments(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addGamers(_ sender: Any) {
+        
+    }
+    
 }
