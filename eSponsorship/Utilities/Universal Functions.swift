991fc5d4774842e328012c090d783f25aa5c1453
Jeremy Tay <j.taytshungman@gmail.com>
2017-10-04 12:31:38 +0800

    Auto stash before merge of "master" and "origin/master"

--- eSponsorship/Utilities/Universal Functions.swift
+++ eSponsorship/Utilities/Universal Functions.swift
@@ -10,9 +10,12 @@ import Foundation
 import UIKit
 
 public class PromptHandler {
+    
     class func showPrompt (title: String, message: String, in vc: UIViewController) {
+        
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
         vc.present(alert, animated: true, completion: nil)
+        
     }
 }
