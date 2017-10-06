bd556cb0ab0907791518aae2cd9fc918b93ee649
Salem Abdulla <salemabdulla@Salems-MacBook-Pro.local>
2017-10-06 12:53:20 +0800

    creation of Profile Page

--- eSponsorship/Controllers/Home Controllers/MenuListViewController.swift
+++ eSponsorship/Controllers/Home Controllers/MenuListViewController.swift
@@ -33,23 +33,14 @@ class MenuListViewController: UIViewController {
         present(vc, animated: true, completion: nil)
         
     }
-    /*
-    @IBAction func addGamers(_ sender: Any) {
-        let storyboardNew = UIStoryboard(name: "Adding", bundle: nil)
-        guard let vc = storyboardNew.instantiateViewController(withIdentifier: "addGamersViewController") as? addGamersViewController else {
-            return
-        }
-        present(vc, animated: true, completion: nil)
-        
-    }
     
-    @IBAction func aboutSponsors(_ sender: Any) {
-        let storyboardNew = UIStoryboard(name: "Adding", bundle: nil)
-        guard let vc = storyboardNew.instantiateViewController(withIdentifier: "AboutViewController") as? AboutViewController else {
-            return
-        }
-        present(vc, animated: true, completion: nil)
-    }
-    
-    
+     @IBAction func addGamers(_ sender: Any) {
+     let storyboardNew = UIStoryboard(name: "Adding", bundle: nil)
+     guard let vc = storyboardNew.instantiateViewController(withIdentifier: "profileViewController") as? profileViewController else {
+     return
+     }
+     present(vc, animated: true, completion: nil)
+     
+     }
 }
+
