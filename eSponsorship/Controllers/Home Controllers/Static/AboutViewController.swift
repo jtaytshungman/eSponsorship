cf93af3397939a04734493089aae838127de0d2b
Jeremy Tay <j.taytshungman@gmail.com>
2017-10-09 15:39:07 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ eSponsorship/Controllers/Home Controllers/Static/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  eSponsorship
+//
+//  Created by Jeremy Tay on 09/10/2017.
+//  Copyright © 2017 Jeremy Tay. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
